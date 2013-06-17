437301996be8a63dbeda5f5b04cd227808bdf212
Josue Palmerin <jpalmerin@nueone.com>
2013-06-17 08:37:47 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrimeApi::Application.config.secret_token = '2e209c6f16a12ed94710f1decdefebeb5ab838d1b6cdb13d357f5cb0f3c634d1500c7da3e522e4c87ba51d5a11b07179fe448cc815570aa3d2d90fad40302e6e'
